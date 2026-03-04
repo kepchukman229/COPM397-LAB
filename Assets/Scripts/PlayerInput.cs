39bffe364f3fee3e63ad05ce8c9c13f3e4baaef2
kepchukman229 <erikraft2006@gmail.com>
2026-03-04 15:45:59 -0500

    Lab#2
    -Player input
    -Player controller

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,20 +1,55 @@
+using System;
+using UnityEditor.ShaderKeywordFilter;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using KBCore.Refs;
+using Unity.Mathematics;
+
+[RequireComponent(typeof(CharacterController))]
 
 public class PlayerInput : MonoBehaviour
 {
     
-    private InputAction inputs;
+    private InputAction move;
+    private InputAction look;
+    [SerializeField] private float maxSpeed = 10.0f;
+    [SerializeField] private float gravity = -30.0f;
+    [SerializeField] private float rotationSpeed = 4.0f;
+    [SerializeField] private float mouseSensY = 5.0f;
+    private Vector3 velocity;
+    [SerializeField, Self] private CharacterController controller;
+    [SerializeField, Child] private Camera cam;
+
+    private void OnValidate()
+    {
+        this.ValidateRefs();
+    }
 
     void Start()
     {
         move = InputSystem.actions.FindAction("Player/Move");
+        look = InputSystem.actions.FindAction("Player/Look");
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 readMove = move.ReadValue<Vector2>();
-        Debug.Log(readMove);    
+        Vector2 readLook = look.ReadValue<Vector2>();
+
+        //Movement of the player
+        Vector3 movement = transform.right * readMove.x + transform.forward * readMove.y;
+        velocity.y += gravity * Time.deltaTime;
+        movement *= maxSpeed * Time.deltaTime;
+        movement += velocity;
+        controller.Move(movement);
+
+        //Rotation of the player
+        transform.Rotate(Vector3.up, readLook.x * rotationSpeed * Time.deltaTime);
+
+        //Rotation of the camera
+        mouseSensY = mouseSensY * readLook.y;
+        mouseSensY = Mathf.Clamp(mouseSensY, -90f, 90f);
+        cam.gameObject.transform.localRotation = Quaternion.Euler(mouseSensY, 0, 0);
     }
 }
